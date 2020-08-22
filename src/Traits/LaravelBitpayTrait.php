baab1220cd0eb813420b013a9aedd4cf4a8c0a0d
Ziga Grcar <ziga.grcar@outlook.com>
2020-08-22 03:27:08 +0200

    Fix typo

--- src/Traits/LaravelBitpayTrait.php
+++ src/Traits/LaravelBitpayTrait.php
@@ -17,9 +17,9 @@ trait LaravelBitpayTrait
             'testnet' == $this->config['network'] ? Env::Test : Env::Prod,
             $this->config['private_key'],
             new Tokens(
-                $this->config['token'], //merchant
+                $this->config['token']//merchant
             ),
-            $this->config['key_storage_password'] //used to decrypt your private key, if encrypted
+            $this->config['key_storage_password']//used to decrypt your private key, if encrypted
         );
     }
 
